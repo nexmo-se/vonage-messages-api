98c4aa466446232040a3aeb62ade527b2ca8add3
Kitt <kittphi@gmail.com>
2022-11-15 11:58:38 -0700

    [1] starting point

---
+++ index.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+let app = express();
+dotenv.config();
+let port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express Server!');
+});
+
+app.post('/webhooks/inbound', (req: Request, res: Response) => {
+  console.log('/inbound req.body', req.body);
+});
+
+app.post('/webhooks/status', (req: Request, res: Response) => {
+  console.log('/status req.body:', req.body);
+  res.status(200).end();
+});
+
+app.post('/webhooks/event', (req: Request, res: Response) => {
+  console.log('/event req.body:', req.body);
+  res.status(200).end();
+});
+
+app.listen(port, () => {
+  console.log(`🌏 Server running at http://localhost:${port}`);
+});
